ceb725b476f352060e007956ce0ea7ee6ce1b562
DESKTOP-JEV71OQ\Rurarz <olekrurarz@gmail.com>
2016-09-09 14:11:20 +0200

    Defaulf route shows all articles

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'ArticlesController@index');
 
 Route::get('test', function () {
     return view('welcome');
