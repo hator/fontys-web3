b4bf91b5fbec56e7566c7f513209b75537dcfe54
Jan Michalski <j.michalski@student.fontys.nl>
2016-09-09 13:56:16 +0200

    Add index function for ArticlesController

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -6,10 +6,21 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
-use App\Article; 
+use App\Article;
 
 class ArticlesController extends Controller
 {
+  /**
+   * Display all articles
+   *
+   * @return Response
+   */
+  public function index()
+  {
+    $articles = Article::all();
+    return view('articles.index', array('articles' => $articles));
+  }
+
   /**
    * Display the specified resource.
    *
