aa39cd04d2df3c478dfbb046b6672157ea15427f
Jan Michalski <j.michalski@student.fontys.nl>
2016-09-09 13:34:02 +0200

    Change Article content field to be text instead of string

--- database/migrations/2016_09_06_100827_create_articles_table.php
+++ database/migrations/2016_09_06_100827_create_articles_table.php
@@ -16,7 +16,7 @@ class CreateArticlesTable extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
-            $table->string('content');
+            $table->text('content');
             $table->string('author');
             $table->timestamps();
         });
