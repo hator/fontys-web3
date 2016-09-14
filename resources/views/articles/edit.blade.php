77f97c747cd76452837f9e058cb224876d909ac7
Jan Michalski <j.michalski@student.fontys.nl>
2016-09-14 19:12:10 +0200

    Fix article edit view

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -1,24 +1,23 @@
 @extends('layouts.master')
 
+@section('title', 'Edit article #' . $article->id . ' ' . $article->title)
+
 @section('content')
 {{ Form::model($article, array('route' => array('articles.update', $article->id), 'method' => 'PUT')) }}
+    <div class="form-group">
+        {!! Form::label('title', 'Title:', ['class' => 'control-label']) !!}
+        {!! Form::text('title', null, ['class' => 'form-control']) !!}
+    </div>
 
-<div class="form-group">
-    {!! Form::label('title', 'Title:', ['class' => 'control-label']) !!}
-    {!! Form::text('title', null, ['class' => 'form-control']) !!}
-</div>
-
-<div class="form-group">
-    {!! Form::label('content', 'Content:', ['class' => 'control-label']) !!}
-    {!! Form::textarea('content', null, ['class' => 'form-control']) !!}
-</div>
+    <div class="form-group">
+        {!! Form::label('content', 'Content:', ['class' => 'control-label']) !!}
+        {!! Form::textarea('content', null, ['class' => 'form-control']) !!}
+    </div>
+    {!! Form::submit('Accept', ['class' => 'btn btn-primary']) !!}
+{!! Form::close() !!}
 
 {{ Form::open(array('url' => 'articles/' . $article->id, 'class' => 'pull-right')) }}
     {{ Form::hidden('_method', 'DELETE') }}
     {{ Form::submit('Delete this article', array('class' => 'btn btn-warning')) }}
 {{ Form::close() }}
-
-{!! Form::submit('Accept', ['class' => 'btn btn-primary']) !!}
-
-{!! Form::close() !!}
 @endsection
