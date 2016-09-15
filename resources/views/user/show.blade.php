f861b36b336a7e7604869c2617fdfd30032e8b16
Jan Michalski <j.michalski@student.fontys.nl>
2016-09-15 20:03:19 +0200

    Add user info and edit button to user profile

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -10,8 +10,14 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in as user {{$user -> id}}
+                    You are logged in as user {{ $user->id }}
+                    <ul>
+                        <li>Name: <strong>{{ $user->name }}</strong></li>
+                        <li>Email: <strong>{{ $user->email }}</strong></li>
+                    </ul>
                 </div>
+
+                <a class="btn btn-warning" href="{{ action('ProfileController@edit', ['id' => $user->id]) }}">Edit your profile</a>
             </div>
         </div>
     </div>
