35f72623d884730f0995043759a056fed4062f2b
Jan Michalski <j.michalski@student.fontys.nl>
2016-09-15 19:43:37 +0200

    Add navbar buttons

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,8 +14,27 @@
     <body>
         <div class="flex-center position-ref full-height">
             <div class="top-right links">
-                <a class="btn btn-primary" href="{{ url('/login') }}">Login</a>
-                <a class="btn btn-primary" href="{{ url('/register') }}">Register</a>
+                @if (Auth::check())
+                    <a class="btn btn-primary" href="{{ action('ArticlesController@index') }}">List articles</a>
+
+                    @can('create', App\Article::class)
+                        <a class="btn btn-success" href="{{ action('ArticlesController@create') }}">Create new article</a>
+                    @endcan
+
+                    <a href="{{ url('/logout') }}"
+                        class="btn btn-danger"
+                        onclick="event.preventDefault();
+                                 document.getElementById('logout-form').submit();">
+                        Logout
+                    </a>
+
+                    <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
+                @else
+                    <a class="btn btn-primary" href="{{ url('/login') }}">Login</a>
+                    <a class="btn btn-primary" href="{{ url('/register') }}">Register</a>
+                @endif
             </div>
 
             <div class="content">
