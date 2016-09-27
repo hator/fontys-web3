9b7a0def83378b2c15d91af59f7a31cb0f671ffc
Jan Michalski <j.michalski@student.fontys.nl>
2016-09-27 11:19:32 +0200

    Reformat ArticlesController

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -9,87 +9,86 @@ use App\Article;
 
 class ArticlesController extends Controller
 {
-  /**
-   * Display all articles
-   *
-   * @return Response
-   */
-  public function index()
-  {
-    $articles = Article::all();
-    return view('articles.index', array('articles' => $articles));
-  }
+    /**
+     * Display all articles
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $articles = Article::all();
+        return view('articles.index', array('articles' => $articles));
+    }
 
-  /**
-   * Display the specified resource.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function show($id)
-  {
-      $article = Article::find($id);
-      return view('articles.show', array('article' => $article));
-  }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        $article = Article::find($id);
+        return view('articles.show', array('article' => $article));
+    }
 
-  /**
-   * Display the article creation form.
-   *
-   * @return Response
-   */
-  public function create()
-  {
-    $this->authorize('create', Article::class);
-    return view('articles.create');
-  }
+    /**
+     * Display the article creation form.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        $this->authorize('create', Article::class);
+        return view('articles.create');
+    }
 
-  /**
-   * Add new article.
-   *
-   * @param  Request  $request
-   * @return Response
-   */
-  public function store(Request $request)
-  {
-    $this->authorize('create', Article::class);
+    /**
+     * Add new article.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        $this->authorize('create', Article::class);
 
-    // TODO Validate the request...
+        // TODO Validate the request...
 
-    $article = new Article;
-    $article->title = $request->title;
-    $article->content = $request->content;
-    Auth::user()->articles()->save($article);
+        $article = new Article;
+        $article->title = $request->title;
+        $article->content = $request->content;
+        Auth::user()->articles()->save($article);
 
-    return redirect()->action('ArticlesController@show', ['id' => $article->id]);
-  }
+        return redirect()->action('ArticlesController@show', ['id' => $article->id]);
+    }
 
-  public function edit($id)
-  {
-    $article = Article::find($id);
-    $this->authorize('update', $article);
+    public function edit($id)
+    {
+        $article = Article::find($id);
+        $this->authorize('update', $article);
 
-    return view('articles.edit', array('article' => $article));
-  }
+        return view('articles.edit', array('article' => $article));
+    }
 
-  public function update(Request $request, $id)
-  {
-     $article = Article::find($id);
-     $this->authorize('update', $article);
-     $article->title = $request->title;
-     $article->content = $request->content;
-     $article->save();
+    public function update(Request $request, $id)
+    {
+        $article = Article::find($id);
+        $this->authorize('update', $article);
+        $article->title = $request->title;
+        $article->content = $request->content;
+        $article->save();
 
-     return redirect()->action('ArticlesController@show', ['id' => $article->id]);
-  }
+        return redirect()->action('ArticlesController@show', ['id' => $article->id]);
+    }
 
-  public function destroy($id)
-  {
-      $article = Article::find($id);
-      $this->authorize('delete', $article);
+    public function destroy($id)
+    {
+        $article = Article::find($id);
+        $this->authorize('delete', $article);
 
-      $article->delete();
+        $article->delete();
 
-      return redirect()->action('ArticlesController@index');
-  }
-
+        return redirect()->action('ArticlesController@index');
+    }
 }
