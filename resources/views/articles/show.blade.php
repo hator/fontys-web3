878c74895f480565f37f8568cd9c9bc57eac7db0
DESKTOP-JEV71OQ\Rurarz <olekrurarz@gmail.com>
2016-10-04 09:53:44 +0200

    Add check if image exist in article page

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -8,7 +8,9 @@
   <div class="article">
     <h1>{{ $article->title }} <span class="article-id">#{{ $article->id }}</span></h1>
     <h3 class="article-author">{{ $article->author->name }}</h3>
+    @if ($article->image_path != "")
     <div>{{ Html::image($article->image_path) }}</div>
+    @endif
     <div class="article-content">{{ $article->content }}</div>
   </div>
   <a class="btn btn-warning" href="{{ action('ArticlesController@edit', ['id' => $article->id]) }}">Edit/delete article</a>
