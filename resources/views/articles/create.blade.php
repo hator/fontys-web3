d407c47a2ef560c402f0412670e61da637eda97d
DESKTOP-JEV71OQ\Rurarz <olekrurarz@gmail.com>
2016-10-04 09:28:19 +0200

    Add select image button to edit and create article page

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -9,10 +9,13 @@
     {!! Form::label('title', 'Title:', ['class' => 'control-label']) !!}
     {!! Form::text('title', null, ['class' => 'form-control']) !!}
 </div>
-
 <div class="form-group">
     {!! Form::label('content', 'Content:', ['class' => 'control-label']) !!}
     {!! Form::textarea('content', null, ['class' => 'form-control']) !!}
+</div>
+<div class="form-group">
+    {!! Form::label('Image') !!}
+    {!! Form::file('image') !!}
 </div>
 
 {!! Form::submit('Create Article', ['class' => 'btn btn-primary']) !!}
--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -8,11 +8,15 @@
         {!! Form::label('title', 'Title:', ['class' => 'control-label']) !!}
         {!! Form::text('title', null, ['class' => 'form-control']) !!}
     </div>
-
     <div class="form-group">
         {!! Form::label('content', 'Content:', ['class' => 'control-label']) !!}
         {!! Form::textarea('content', null, ['class' => 'form-control']) !!}
     </div>
+    <div class="form-group">
+        {!! Form::label('Image') !!}
+        {!! Form::file('image') !!}
+    </div>
+
     {!! Form::submit('Accept', ['class' => 'btn btn-primary']) !!}
 {!! Form::close() !!}
 
