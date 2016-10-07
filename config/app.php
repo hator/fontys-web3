5f594c5c57ad36f8661683fe7234ec51b74e9d1c
DESKTOP-JEV71OQ\Rurarz <olekrurarz@gmail.com>
2016-10-07 13:13:22 +0200

    Add dompdf lib to download content as pdf

--- config/app.php
+++ config/app.php
@@ -179,6 +179,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
 
     ],
 
@@ -231,6 +232,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Image' => Intervention\Image\Facades\Image::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class,
 
     ],
 
